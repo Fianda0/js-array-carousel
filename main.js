9b99f9705b078d8d5aa1787f05af8966b171e0ba
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-04-23 16:35:08 +0200

    M4 - button back

--- main.js
+++ main.js
@@ -38,4 +38,40 @@ document.getElementById('next').addEventListener('click', function () {
 
 })
 
+// inserisco bottone per passare alla slide precedente
 
+document.getElementById('back').addEventListener('click', function () {
+
+
+    // Inizio ciclo for per controllare le immagini
+    for (let i = 0; i < slide.length; i++) {
+
+        const slideControl = slide[i]
+        console.log(slideControl)
+
+        // controllo slide se hanno la classe active
+        if (slideControl.classList.contains('active')) {
+            // se contine ACTIVE lo rimuovo
+            slideControl.classList.remove('active')
+
+            // segno quale array ha la classe ACTIVE
+            imgActive = i;
+
+        }
+    }
+
+    // seleziono la prossima slide da attivare
+    slideNext = imgActive - 1;
+    console.log(slideNext)
+
+    // se mi trovo all'ulitma slide torno alla prima
+    if (slideNext == -1) {
+        slideNext = slide.length;
+    }
+    console.log('lunghezza array', slide.length)
+    console.log(slideNext)
+    console.log(slideNext[slide.length])
+
+    slide[slideNext].classList.add('active')
+
+})
