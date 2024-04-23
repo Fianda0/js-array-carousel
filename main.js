37f10025c2f33691a6049344040d7011518e1e1b
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-04-23 16:03:37 +0200

    M2 - ciclo for per rimuovere Attivazione

---
+++ main.js
@@ -0,0 +1,24 @@
+let imgActive;
+let slideNext;
+
+// Creazione array con immagine delle slide
+let slide = document.getElementsByClassName('slide');
+console.log(slide)
+
+// Inizio ciclo for per controllare le immagini
+for (let i = 0; i < slide.length; i++) {
+
+    const slideControl = slide[i]
+    console.log(slideControl)
+
+    // controllo slide se hanno la classe active
+    if (slideControl.classList.contains('active')) {
+        // se contine ACTIVE lo rimuovo
+        slideControl.classList.remove('active')
+
+        // segno quale array ha la classe ACTIVE
+        imgActive = i;
+
+    }
+
+}
